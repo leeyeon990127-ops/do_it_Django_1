70894900b64d4fefb1fba2aeb6f494a3aab5ac28
yeonsoo <leeyeon990127@gmail.com>
2026-05-18 18:16:16 +0900

    ~p.395 새포스트 작성 버튼 만들기

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 import os
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -11,9 +20,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    class Meta:
-        verbose_name_plural = "Categories"
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
 
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -26,9 +37,9 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
-     
+    tags = models.ManyToManyField(Tag, blank=True)    
+
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
 
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,71 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post, Category, Tag
+from django.shortcuts import render, redirect
 
-# Create your views here.
 class PostList(ListView):
     model = Post
     ordering = '-pk'
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(request, 'blog/post_list.html', {
+        'post_list': post_list,
+        'categories': Category.objects.all(),
+        'no_category_post_count': Post.objects.filter(category=None).count(),
+        'category': category,
+    })
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+        }
+    )
 
 class PostDetail(DetailView):
     model = Post
 
-# def index(request):
-#     posts = Post.objects.all().order_by('-pk') 
+    def get_context_data(self, **kwages):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category',]
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')    
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
 #     return render(
 #         request,
 #         'blog/index.html',
@@ -22,9 +74,10 @@ class PostDetail(DetailView):
 #         }
 #     )
 
+
 # def single_post_page(request, pk):
 #     post = Post.objects.get(pk=pk)
-
+#
 #     return render(
 #         request,
 #         'blog/single_post_page.html',
@@ -32,4 +85,3 @@ class PostDetail(DetailView):
 #             'post': post,
 #         }
 #     )
-
