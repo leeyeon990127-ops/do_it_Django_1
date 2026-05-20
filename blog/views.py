bc18f8a1432d4ae05ae48545669cec461977c320
yeonsoo <leeyeon990127@gmail.com>
2026-05-20 15:44:30 +0900

    ~p. 503  댓글 삭제 기능 구현하기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from .models import Post, Category, Tag
+from django.shortcuts import get_object_or_404
+from .models import Post, Category, Tag, Comment
 from .forms import CommentForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -14,46 +15,17 @@ class PostList(ListView):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
-        context['comment_form'] = CommentForm
+        
         return context
-
-def category_page(request, slug):
-    if slug == 'no_category':
-        category = '미분류'
-        post_list = Post.objects.filter(category=None)
-    else:
-        category = Category.objects.get(slug=slug)
-        post_list = Post.objects.filter(category=category)
-
-    return render(request, 'blog/post_list.html', {
-        'post_list': post_list,
-        'categories': Category.objects.all(),
-        'no_category_post_count': Post.objects.filter(category=None).count(),
-        'category': category,
-    })
-
-def tag_page(request, slug):
-    tag = Tag.objects.get(slug=slug)
-    post_list = tag.post_set.all()
-
-    return render(
-        request,
-        'blog/post_list.html',
-        {
-            'post_list': post_list,
-            'tag': tag,
-            'categories': Category.objects.all(),
-            'no_category_post_count': Post.objects.filter(category=None).count(),
-        }
-    )
 
 class PostDetail(DetailView):
     model = Post
 
-    def get_context_data(self, **kwages):
+    def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
         return context
 
 class PostCreate(LoginRequiredMixin, CreateView):
@@ -101,6 +73,12 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
 
         return context
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs) 
+        else:
+            raise PermissionDenied    
+
     def form_valid(self, form):
         response = super(PostUpdate, self).form_valid(form)
         self.object.tags.clear()
@@ -121,11 +99,62 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
 
         return response                       
 
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(request, 'blog/post_list.html', {
+        'post_list': post_list,
+        'categories': Category.objects.all(),
+        'no_category_post_count': Post.objects.filter(category=None).count(),
+        'category': category,
+    })
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+        }
+    )
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post 
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+            else:
+                return redirect(post.get_absolute_url())
+        else:
+            raise PermissionDenied      
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment 
+    form_class = CommentForm
+
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
-            return super(PostUpdate, self).dispatch(request, *args, **kwargs) 
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)  
         else:
-            raise PermissionDenied            
+            raise PermissionDenied                                              
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
 #
