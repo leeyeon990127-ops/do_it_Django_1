d9afa65ceadfae19a5855ffee6f16e90688c041b
yeonsoo <leeyeon990127@gmail.com>
2026-05-21 18:56:23 +0900

    도커 설치 후 도커 환경에서 웹 띄우기

--- blog/models.py
+++ blog/models.py
@@ -68,7 +68,7 @@ class Comment(models.Model):
         return f'{self.author}::{self.content}'
 
     def get_absolute_url(self):
-        return f'{self.post.get_absolute_url()}#comment-{self.pk}'    
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
 
     def get_avatar_url(self):
         return 'http://placehold.it/50x50'  
